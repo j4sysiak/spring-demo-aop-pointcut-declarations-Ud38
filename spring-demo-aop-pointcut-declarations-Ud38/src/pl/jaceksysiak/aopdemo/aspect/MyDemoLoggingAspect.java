50be6e766ba57ac578b78c915c604763c81ce293
Jacek <Jacek@BERLIN>
2020-03-05 05:00:20 +0100

    cxcxc

--- spring-demo-aop-pointcut-declarations-Ud38/src/pl/jaceksysiak/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations-Ud38/src/pl/jaceksysiak/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,29 +2,26 @@ package pl.jaceksysiak.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-	// this is where we add all of our related advices for logging
+	@Pointcut("execution(* pl.jaceksysiak.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
 	
-	// let's start with an @Before advice
-
-	//@Before("execution(public void addAccount())")	
-	//@Before("execution(public void pl.jaceksysiak.aopdemo.dao.AccountDAO.addAccount())")
-	//@Before("execution(public void add*())")
-	//@Before("execution(void add*())")
-	//@Before("execution(* add*())")
-	//@Before("execution(* add*(pl.jaceksysiak.aopdemo.Account, ..))")   // account and any other param.
-	//@Before("execution(* add*(..))")   //any parameters
-	  @Before("execution(* pl.jaceksysiak.aopdemo.dao.*.*(..))")      // avery thing in given package
-	public void beforeAddAccountAdvice() {
-		
-		System.out.println("\n=====>>> Executing @Before advice on some method");
-		
+	@Before("forDaoPackage()")
+	public void beforeAddAccountAdvice() {		
+		System.out.println("\n=====>>> Executing @Before advice on method");		
 	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> Performing API analytics");		
+	}
+
 }
 
 
